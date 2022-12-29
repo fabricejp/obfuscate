692707e63c0db1dcaf45cdf5c13dc5837cbc8e85
Fabrice De Kimpe <fabrice.dekimpe@Fabrices-MacBook-Pro-3.local>
2022-12-29 11:38:51 +0100

    src

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        panic!("Please provide an email address as the command line argument");
+    }
+
+    let email = &args[1];
+    let obfuscated_email = obfuscate_email(email);
+
+    //println!("Obfuscated email address: {}", obfuscated_email);
+    println!("{}", obfuscated_email);
+}
+
+fn obfuscate_email(email: &String) -> String {
+    let mut obfuscated_email = String::new();
+    let mut at_index = 0;
+
+    for (index, c) in email.chars().enumerate() {
+        if c == '@' {
+            at_index = index;
+            break;
+        }
+    }
+
+    for (index, c) in email.chars().enumerate() {
+        if index < at_index - 2 || index > at_index + 2 {
+            obfuscated_email.push('*');
+        } else {
+            obfuscated_email.push(c);
+        }
+    }
+
+    obfuscated_email
+}
